c363af158a4d9e30aaae70593ad05f54ab337d50
dophambichtram <bichtramdopham@gmail.com>
2023-10-01 21:42:47 +0700

    up

--- Baitap_Day6/src/component/Chart/Chart.jsx
+++ Baitap_Day6/src/component/Chart/Chart.jsx
@@ -2,9 +2,11 @@ import './Chart.css';
 import ChartBar from './ChartBar';
 
 const Chart = (props) => {
-  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-  const totalMaximum = Math.max(...dataPointValues);
 
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  // tạo hàm totalMaximum để gán kết quả hàm Math.max trên mảng dataPointValues
+  const totalMaximum = Math.max(...dataPointValues);
+  // trả về của hàm Chart
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
